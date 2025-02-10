a30690462e713e8576a483198d110052de0f1f54
Angel de Jesus Hernandez Hernndez <Angel.h@Urquiza2020.local>
2025-02-10 09:44:01 -0600

    Seeder Maestro

---
+++ .history/app/Database/Seeds/DatabaseSeeder_20250210094151.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        $this->call('UsuariosSeeder');
+        $this->call('AlumnosSeeder');
+        $this->call('MaestrosSeeder');
+        $this->call('GruposSeeder');
+        $this->call('TareasSeeder');
+        $this->call('CalificacionesSeeder');
+    }
+}
---
+++ .history/app/Database/Seeds/DatabaseSeeder_20250210094254.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        $this->call('UsuarioSeeder');
+        $this->call('AlumnoSeeder');
+        $this->call('MaestroSeeder');
+        $this->call('GrupoSeeder');
+        $this->call('TareaSeeder');
+        // $this->call('CalificacionesSeeder');
+    }
+}
